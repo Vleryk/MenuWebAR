0ff363b6cfca0c841ad8da4097c721f840aa331f
Javier Garrido <garridocortesjavier@gmail.com>
2026-04-01 15:46:39 -0300

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import AdminDashboard from "./admin/AdminDashboard";
 import "./globals.css";
+import DirectARViewer from "./components/DirectARViewer"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/ar/:itemId" element={<DirectARViewer />} /> 
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
