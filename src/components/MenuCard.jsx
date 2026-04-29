b93d3a2a00c2cca2b0670a772230562e23757251
Sebastián Vergara Muñoz <sa.vergaramunoz@gmail.com>
2026-04-29 16:47:27 -0400

    refactor: move CameraIcon and IngredientsIcon to separate files for better modularity

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -16,24 +16,8 @@
 import { useEffect, useRef, useState } from "react";
 import styles from "./MenuCard.module.css";
 import { currencyFormatter } from "../config/currencyFormatter";
-
-// Icono de camara (SVG inline para evitar una dependencia extra por un icono).
-function CameraIcon() {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" className={styles.cameraIcon}>
-      <path d="M9 4.5a1 1 0 0 0-.8.4l-1 1.3H5A3 3 0 0 0 2 9.2v7.3a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V9.2a3 3 0 0 0-3-3h-2.2l-1-1.3a1 1 0 0 0-.8-.4H9Zm3 4a4.8 4.8 0 1 1 0 9.6 4.8 4.8 0 0 1 0-9.6Zm0 1.9a2.9 2.9 0 1 0 0 5.8 2.9 2.9 0 0 0 0-5.8Z" />
-    </svg>
-  );
-}
-
-// Icono de lista de ingredientes.
-function IngredientsIcon() {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" className={styles.cameraIcon}>
-      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z" />
-    </svg>
-  );
-}
+import { CameraIcon } from "./icons/CameraIcon";
+import { IngredientsIcon } from "./icons/IngredientsIcon";
 
 function MenuCard({ item }) {
   const [isArOpen, setIsArOpen] = useState(false);
