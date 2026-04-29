c4af2c625e76395254fbb74773193dac71e41675
Sebastián Vergara Muñoz <sa.vergaramunoz@gmail.com>
2026-04-29 16:16:36 -0400

    feat: add social media icons for Facebook and Instagram in Header and Footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,8 @@
 import { useEffect, useState } from "react";
 import restaurantConfig from "../config/restaurant";
 import styles from "./Footer.module.css";
+import { IconFacebook } from "./icons/IconFacebook";
+import { IconInstagram } from "./icons/IconInstagram";
 
 function Footer() {
   // Estado para saber si el restaurante esta abierto en este momento
@@ -55,20 +57,12 @@ function Footer() {
     {
       name: "Instagram",
       url: restaurantConfig.social.instagram,
-      icon: (
-        <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-          <path d="M7.5 2h9A5.5 5.5 0 0 1 22 7.5v9a5.5 5.5 0 0 1-5.5 5.5h-9A5.5 5.5 0 0 1 2 16.5v-9A5.5 5.5 0 0 1 7.5 2Zm0 1.8A3.7 3.7 0 0 0 3.8 7.5v9a3.7 3.7 0 0 0 3.7 3.7h9a3.7 3.7 0 0 0 3.7-3.7v-9a3.7 3.7 0 0 0-3.7-3.7h-9Zm9.35 1.35a1.3 1.3 0 1 1 0 2.6 1.3 1.3 0 0 1 0-2.6ZM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 1.8a3.2 3.2 0 1 0 0 6.4 3.2 3.2 0 0 0 0-6.4Z" />
-        </svg>
-      ),
+      icon: <IconInstagram />,
     },
     {
       name: "Facebook",
       url: restaurantConfig.social.facebook,
-      icon: (
-        <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-          <path d="M13.5 22v-8.3h2.8l.4-3.3h-3.2V8.3c0-1 .3-1.7 1.8-1.7h1.5V3.7c-.3 0-1.1-.1-2.2-.1-2.2 0-3.7 1.4-3.7 4v2.8H8.5v3.3h2.4V22h2.6Z" />
-        </svg>
-      ),
+      icon: <IconFacebook />,
     },
   ];
 
